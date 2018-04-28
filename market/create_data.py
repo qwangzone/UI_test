ecf4f3f93cc5ad22e997034a574d91e9dada5dee
qwangzone <578380132@qq.com>
2018-04-28 17:56:42 +0800

    完善代码

--- market/create_data.py
+++ market/create_data.py
@@ -1,18 +1,33 @@
 import os, sys
+
 dir = os.path.dirname(os.path.dirname(__file__))
-sys.path.append(dir+"/manage/pages")
+sys.path.append(dir + "/manage/pages")
 from createprojectpage import CreateNew
 from loginpage import LoginPage
 from selenium import webdriver
+from datetime import datetime
+import random
+
+
 # from loanuserinfopage import LoanUserInfo
 # from uploadimgpage import UploadImg
 # from projectstatuspage import ProjectSta
+def num():
+    for i in range(1, 1000):
+        yield i
+def projectname():
+    now = datetime.now().strftime("%m%d")
+    f = num()
+    return now + str(next(f))
+
+
+
 
 def createdata(driver):
     login_p = LoginPage(driver)
     create_p = CreateNew(driver)
 
-    #登录操作
+    # 登录操作
     login_p.open()
     login_p.login()
 
@@ -31,6 +46,12 @@ def createdata(driver):
     # 改变项目状态
     prosta_p = create_p.projectstatus()
     prosta_p.changeprosta()
+
+    # 获取标id
+    url = driver.current_url
+    project_id = url.split("/")[-1]
+    return project_id
+
 
 if __name__ == '__main__':
     dr = webdriver.Chrome()
