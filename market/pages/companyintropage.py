f31ff99418a1889fef09a02d65f153c5a3fba438
qwangzone <578380132@qq.com>
2018-05-07 17:56:21 +0800

    代码update

--- market/pages/companyintropage.py
+++ market/pages/companyintropage.py
@@ -1,1 +1,11 @@
 '''公司简介'''
+from basepage import BasePage
+class CompanyInfoPage(BasePage):
+    url = "/information/informationPublic"
+
+    @property
+    def content(self):
+        return self.by_class_name("xf_mainRight")
+
+    def get_content(self):
+        return self.content.get_attribute("innerHTML")
